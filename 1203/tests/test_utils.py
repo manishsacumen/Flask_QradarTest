443c7a20be30dce6ad8a46e46e1bfd91ce6660aa
Deepak Baraik <deepak.baraik@sacumen.com>
2019-12-10 19:30:27 +0530

    Test cases added for Utils and views

--- 1203/tests/test_utils.py
+++ 1203/tests/test_utils.py
@@ -1,13 +1,36 @@
 from flask import Flask
 from app import app
+from app.utils import validate_api_credentials, get_json_from_url
+from mock import MagicMock, patch
+import requests
+from app import app
+import requests_mock
+import mock
+from mock_data import JSON_DATA
+import json
+
+class TestUtilsObject(object):
+
+    def test_validate_api_credentials(self):
+
+        url = "https://api.securityscorecard.io"
+        token = "gAAAAABd5PaRpM3BxeTZl4L-tOU6Hszd_yo7zWSpQufwm2SaQITczdA"
+
+        validate_api_credentials = MagicMock(return_value=[200, 429])
+
+        if validate_api_credentials.status_code in [200, 429]:
+            assert validate_api_credentials(url, token) == True
+
+    @requests_mock.mock()
+    def test_get_json_from_url(self, mock):
+
+        mock.get("http://123-fake-api.com", text=json.dumps(JSON_DATA))
+
+        response = get_json_from_url("http://123-fake-api.com", headers=None, params=None, proxy=None)
+
+        assert response == {"key": "value"}
 
 
-def test_base_route():
-    # app = Flask(__name__)
-    # configure_routes(app)
-    client = app.test_client()
-    url = '/'
 
-    response = client.get(url)
 
-    assert response.status_code == 200
+
--- 1203/tests/test_views.py
+++ 1203/tests/test_views.py
@@ -1,25 +1,4 @@
-# from test_app import MyTest
-# from data_store import read_data_store
-#
-# current_config_access_key = {'access_key': 'xyz'}
-# current_config_port_list = ['abc123', 'asd23d']
-#
-# app = MyTest.create_app()
-#
-#
-# @app.route('/', methods=['GET', 'POST'])
-# @app.route('/index')
-# def test_index_with_access_key():
-#     access_key = 'xyz'
-#     assert access_key == current_config_access_key['access_key']
-#
-#
-# @app.route('/', methods=['GET', 'POST'])
-# @app.route('/index')
-# def test_index_with_portfolio_list():
-#     portfolio_list = ['abc123', 'asd23d']
-#     assert portfolio_list == current_config_port_list
-#
+import flask
 from flask import Flask
 from mock import MagicMock
 import mock
@@ -27,11 +6,40 @@ from app import views
 import pytest
 from conftest import client
 from app.data_store import read_data_store
+import json
+from flask import template_rendered
+from contextlib import contextmanager
+from app.flask_keys import get_flask_keys, random_word
+import responses
+import requests
+from pytest_mock import mocker
+from app import app, views
+from mock_data import RESULT_DATA, RESULT_DATA1
+# def test_index(client):
+#     # read_data_store = MagicMock(return_value = {"Hello":"Hi"})
+#     url = '/' or '/index'
+#     response = client.get(url)
+#     assert response.status_code == 200
 
 
-def test_index(client, mocker):
-    read_data_store = MagicMock(return_value = {"Hello":"Hi"})
-    url = '/' or '/index'
-    response = client.get(url)
-    assert response.status_code == 200
+# def test_result2(client, mocker):
+#     from app.qpylib import qpylib
+#     qpylib.get_console_address = MagicMock(return_value=None)
+#     qpylib.log = MagicMock(return_value=None)
+#
+#     data = {'secret_key': random_word(), 'token': random_word()}
+#
+#     mocker.patch('app.flask_keys.get_flask_keys', return_value=data)
+#     mocker.patch('app.views.render_template', return_value="Get Called")
+#     mocker.patch('app.poll_n_write.poll_scorecard_n_write', return_value=True)
+#     mocker.patch('app.poll_n_write.poll_scorecard_n_write', return_value=None)
+#     RESULT_DATA1.update(({"csrf_token": False}))
+#
+#     request_mock = mocker.patch.object(flask, "request")
+#     request_mock.form.get.return_value = RESULT_DATA1
+#     url = '/result'
+#
+#     response, msg = client.post(url, data=json.dumps(RESULT_DATA1))
+
+    # assert response.status_code == 403
 
